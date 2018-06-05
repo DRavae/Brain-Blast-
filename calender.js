ee650d905d47bb22176125f8ee840c997e8e96b1
Donte Ravae <36210188+DRavae@users.noreply.github.com>
2018-06-05 15:54:12 -0400

    Create calender.js

---
+++ calender.js
@@ -0,0 +1,5 @@
+const base = new Date();
+const currentMonth = base.getMonth();
+const currentDay = base.getDate();
+const currentYear = base.getFullYear();
+const today = currentMonth + '-' + currentDay + '-' + currentYear;
