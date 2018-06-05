28104360406e7523674dabcb20020928abaeabee
Donte Ravae <36210188+DRavae@users.noreply.github.com>
2018-06-05 16:56:16 -0400

    Update calender.js

--- calender.js
+++ calender.js
@@ -3,3 +3,8 @@ const currentMonth = base.getMonth();
 const currentDay = base.getDate();
 const currentYear = base.getFullYear();
 const today = currentMonth + '-' + currentDay + '-' + currentYear;
+
+class Calender {
+  constructor(){}
+  this._month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+}
